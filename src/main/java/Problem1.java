a63975be113e15a55ec867aa337948467107bd5f
Chum <ravichum@gmail.com>
2020-10-15 09:35:32 -0700

    First Commit

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -6,5 +6,16 @@ public class Problem1 {
 
     // Do not change signature (function name, parameters type, and return type)
     public static void colorSort(int[] nums) {
+        int start = 0;
+        int cur = 0;
+        int end = nums.length-1;
+
+        while (cur <= end){
+            if(nums[cur] == 0){
+                int temp = nums[cur];
+                nums[cur] = nums[start];
+                nums[start] = temp;
+            }
+        }
     }
 }
